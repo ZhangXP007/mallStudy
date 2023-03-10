a45592cffc504ab4de50200bd5e8c8951ab048c2
zhangxp-pc <zxp15851803991@163.com>
2023-03-10 09:10:52 +0800

    test01

---
+++ demo/src/main/java/com/mall/controller/DemoController.java
@@ -0,0 +1,18 @@
+package com.mall.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Author: Zxp
+ * @Description:
+ * @Version: 1.0
+ */
+@RestController
+public class DemoController {
+    @RequestMapping("/index")
+
+    public String index(){
+        return "hello SpringBoot!";
+    }
+}
